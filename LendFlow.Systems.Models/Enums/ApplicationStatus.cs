6756bee49beea8075582532ecd9b515d8dd43c0e
Abhijith Sukumar <82198607+AbhijithSukumar@users.noreply.github.com>
2026-06-05 14:24:22 +0530

    Create ApplicationStatus.cs

---
+++ LendFlow.Systems.Models/Enums/ApplicationStatus.cs
@@ -0,0 +1,10 @@
+namespace LendFlow.Systems.Models.Enums
+{
+    public enum ApplicationStatus
+    {
+        Submitted = 1,
+        UnderReview = 2,
+        Approved = 3,
+        Rejected = 4
+    }
+}

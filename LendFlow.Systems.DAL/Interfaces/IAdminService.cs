3d5ad86c501680acdc831c9c02eb5738a1a4d3ac
Abhijith Sukumar <82198607+AbhijithSukumar@users.noreply.github.com>
2026-06-05 14:13:16 +0530

    Add files via upload

---
+++ LendFlow.Systems.DAL/Interfaces/IAdminService.cs
@@ -0,0 +1,10 @@
+﻿using LendFlow.Systems.Models.DTOs;
+
+namespace LendFlow.Systems.BLL.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<int> RegisterAdminAsync(StaffRegisterRequestDTO request);
+        Task<string?> LoginAsync(StaffLoginRequestDTO request);
+    }
+}
---
+++ LendFlow.Systems.DAL/Interfaces/ICreditService.cs
@@ -0,0 +1,17 @@
+﻿using LendFlow.Systems.Models.DTOs;
+
+
+namespace LendFlow.Systems.BLL.Interfaces
+{
+    public interface ICreditService
+    {
+        // For Borrowers submitting applications
+        Task<int> SubmitLoanApplicationAsync(int userId, LoanApplicationRequestDTO request);
+
+        // For Credit Underwriters retrieving pending work
+        Task<IEnumerable<LoanApplicationSummaryDTO>> GetPendingApplicationsAsync();
+
+        // For Credit Underwriters processing evaluations
+        Task<bool> ProcessLoanEvaluationAsync(int underwriterStaffId, int applicationId, CreditEvaluationRequestDTO request);
+    }
+}
